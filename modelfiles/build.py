eb8644b2c574cb07dd3f30377bbd5fc737459892
Kyler Larsen <63459392+Aopsmath99@users.noreply.github.com>
2022-08-05 15:18:55 -0400

    Update build.py

--- modelfiles/build.py
+++ modelfiles/build.py
@@ -4,6 +4,7 @@
 # Licensed under The MIT License [see LICENSE for details]
 # Written by Ze Liu
 # Modified by Zhenda Xie
+# Further modified by Kyler Larsen
 # --------------------------------------------------------
 
 from .swin_transformer import build_swin
