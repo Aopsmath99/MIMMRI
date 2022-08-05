0fffc9b3d4f75bdbecfb5c0cb33164b1ec297b72
Aopsmath99 <kv452@pnl-oracle.bwh.harvard.edu>
2022-08-05 14:03:09 -0400

    primary driver codes, mostly unmodified except for data collection

---
+++ simmim_main.py
@@ -0,0 +1,320 @@
+# --------------------------------------------------------
+# SimMIM
+# Copyright (c) 2021 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ze Liu
+# Modified by Zhenda Xie
+# --------------------------------------------------------
+
+import os
+import time
+import argparse
+import datetime
+import numpy as np
+import PIL
+import torch
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+from timm.utils import AverageMeter
+import torchvision.transforms as T
+from config import get_config
+from models import build_model
+from data import build_loader
+from lr_scheduler import build_scheduler
+from optimizer import build_optimizer
+from logger import create_logger
+from utils import load_checkpoint, save_checkpoint, get_grad_norm, auto_resume_helper
+from PIL import Image, ImageOps
+import math 
+#import amp
+
+'''
+try:
+    # noinspection PyUnresolvedReferences
+    from apex import amp
+except ImportError:
+    amp = None
+'''
+
+def parse_option():
+    parser = argparse.ArgumentParser('SimMIM pre-training script', add_help=False)
+    parser.add_argument('--cfg', type=str, required=True, metavar="FILE", help='path to config file', )
+    parser.add_argument(
+        "--opts",
+        help="Modify config options by adding 'KEY VALUE' pairs. ",
+        default=None,
+        nargs='+',
+    )
+
+    # easy config modification
+    parser.add_argument('--batch-size', type=int, help="batch size for single GPU")
+    parser.add_argument('--data-path', type=str, help='path to dataset')
+    parser.add_argument('--resume', help='resume from checkpoint')
+    parser.add_argument('--accumulation-steps', type=int, help="gradient accumulation steps")
+    parser.add_argument('--use-checkpoint', action='store_true',
+                        help="whether to use gradient checkpointing to save memory")
+    parser.add_argument('--amp-opt-level', type=str, default='O1', choices=['O0', 'O1', 'O2'],
+                        help='mixed precision opt level, if O0, no amp is used')
+    parser.add_argument('--output', default='output', type=str, metavar='PATH',
+                        help='root of output folder, the full path is <output>/<model_name>/<tag> (default: output)')
+    parser.add_argument('--tag', help='tag of experiment')
+
+    # distributed training
+    parser.add_argument("--local_rank", type=int, required=True, help='local rank for DistributedDataParallel')
+
+    args = parser.parse_args()
+
+    config = get_config(args)
+
+    return args, config
+
+
+def main(config):
+    data_loader_train = build_loader(config, logger, is_pretrain=True, train = True)
+    data_loader_val = build_loader(config, logger, is_pretrain=True, train = False)
+    logger.info(f"Creating model:{config.MODEL.TYPE}/{config.MODEL.NAME}")
+    model = build_model(config, is_pretrain=True)
+    model.cuda()
+    logger.info(str(model))
+
+    optimizer = build_optimizer(config, model, logger, is_pretrain=True)
+    if config.AMP_OPT_LEVEL != "O0":
+        model, optimizer = amp.initialize(model, optimizer, opt_level=config.AMP_OPT_LEVEL)
+    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[config.LOCAL_RANK], broadcast_buffers=False)
+    model_without_ddp = model.module
+
+    n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    logger.info(f"number of params: {n_parameters}")
+    if hasattr(model_without_ddp, 'flops'):
+        flops = model_without_ddp.flops()
+        logger.info(f"number of GFLOPs: {flops / 1e9}")
+
+    lr_scheduler = build_scheduler(config, optimizer, len(data_loader_train))
+
+    if config.TRAIN.AUTO_RESUME:
+        resume_file = auto_resume_helper(config.OUTPUT, logger)
+        if resume_file:
+            if config.MODEL.RESUME:
+                logger.warning(f"auto-resume changing resume file from {config.MODEL.RESUME} to {resume_file}")
+            config.defrost()
+            config.MODEL.RESUME = resume_file
+            config.freeze()
+            logger.info(f'auto resuming from {resume_file}')
+        else:
+            logger.info(f'no checkpoint found in {config.OUTPUT}, ignoring auto resume')
+
+    if config.MODEL.RESUME:
+        load_checkpoint(config, model_without_ddp, optimizer, lr_scheduler, logger)
+
+    logger.info("Start training")
+    start_time = time.time()
+    for epoch in range(config.TRAIN.START_EPOCH, config.TRAIN.EPOCHS):
+        data_loader_train.sampler.set_epoch(epoch)
+        loss_array = []
+        grad_array = []
+        ssim_array = []
+        f1_array = []
+        train_one_epoch(config, model, data_loader_train, optimizer, epoch, lr_scheduler, loss_array, grad_array, ssim_array)
+        if dist.get_rank() == 0 and (epoch % config.SAVE_FREQ == 0 or epoch == (config.TRAIN.EPOCHS - 1)):
+            save_checkpoint(config, epoch, model_without_ddp, 0., optimizer, lr_scheduler, logger)
+        logger2.info('EPOCH NUMBER : ' + str(epoch))
+        logger2.info('Training max and min loss: ' + str(truncate(max(loss_array), 4)) + ' ' + str(truncate(min(loss_array), 4)))
+        logger2.info('Training max and min grad: ' + str(truncate(max(grad_array), 4)) + ' ' + str(truncate(min(grad_array), 4)))
+        logger2.info('Training max and min ssim: ' + str(truncate(max(ssim_array), 4)) + ' ' + str(truncate(min(ssim_array), 4)))
+        loss_array = []
+        ssim_array = [] 
+        f1_array = []       
+        loss, ssim= validate(config, data_loader_val, model, loss_array, ssim_array)
+        logger2.info('Validation max and min loss: ' + str(truncate(max(loss_array), 4)) + ' ' + str(truncate(min(loss_array), 4)))
+        logger2.info('Validation max and min ssim: ' + str(truncate(max(ssim_array), 4)) + ' ' + str(truncate(min(ssim_array), 4)))
+        
+    total_time = time.time() - start_time
+    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+    logger.info('Training time {}'.format(total_time_str))
+
+def tensor_to_image(tensor):
+    tensor = tensor*255
+    tensor = np.array(tensor, dtype=np.uint8)
+    if np.ndim(tensor)>3:
+        assert tensor.shape[0] == 1
+        tensor = tensor[0]
+    return PIL.Image.fromarray(tensor)
+def truncate(number, digits) -> float:
+    try:
+        nbDecimals = len(str(number).split('.')[1]) 
+        if nbDecimals <= digits:
+            return number
+        stepper = 10.0 ** digits
+        return math.trunc(stepper * number) / stepper
+    except:
+        return number
+
+def train_one_epoch(config, model, data_loader, optimizer, epoch, lr_scheduler, l, g, s):
+    model.train()
+    optimizer.zero_grad()
+
+    num_steps = len(data_loader)
+    batch_time = AverageMeter()
+    loss_meter = AverageMeter()
+    norm_meter = AverageMeter()
+    ssim_meter = AverageMeter()
+    #f1_meter = AverageMeter()
+    start = time.time()
+    end = time.time()
+    for idx, (img, mask, _) in enumerate(data_loader):
+        img = img.cuda(non_blocking=True)
+        mask = mask.cuda(non_blocking=True)
+        p = []
+        target = []
+        loss, ssim_score = model(img, mask, target, p)
+        if config.TRAIN.ACCUMULATION_STEPS > 1:
+            loss = loss / config.TRAIN.ACCUMULATION_STEPS
+            if config.AMP_OPT_LEVEL != "O0":
+                with amp.scale_loss(loss, optimizer) as scaled_loss:
+                    scaled_loss.backward()
+                if config.TRAIN.CLIP_GRAD:
+                    grad_norm = torch.nn.utils.clip_grad_norm_(amp.master_params(optimizer), config.TRAIN.CLIP_GRAD)
+                else:
+                    grad_norm = get_grad_norm(amp.master_params(optimizer))
+            else:
+                loss.backward()
+                if config.TRAIN.CLIP_GRAD:
+                    grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), config.TRAIN.CLIP_GRAD)
+                else:
+                    grad_norm = get_grad_norm(model.parameters())
+            if (idx + 1) % config.TRAIN.ACCUMULATION_STEPS == 0:
+                optimizer.step()
+                optimizer.zero_grad()
+                lr_scheduler.step_update(epoch * num_steps + idx)
+        else:
+            optimizer.zero_grad()
+            if config.AMP_OPT_LEVEL != "O0":
+                with amp.scale_loss(loss, optimizer) as scaled_loss:
+                    scaled_loss.backward()
+                if config.TRAIN.CLIP_GRAD:
+                    grad_norm = torch.nn.utils.clip_grad_norm_(amp.master_params(optimizer), config.TRAIN.CLIP_GRAD)
+                else:
+                    grad_norm = get_grad_norm(amp.master_params(optimizer))
+            else:
+                loss.backward()
+                if config.TRAIN.CLIP_GRAD:
+                    grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), config.TRAIN.CLIP_GRAD)
+                else:
+                    grad_norm = get_grad_norm(model.parameters())
+            optimizer.step()
+            lr_scheduler.step_update(epoch * num_steps + idx)
+
+        torch.cuda.synchronize()
+
+        loss_meter.update(loss.item(), img.size(0))
+        norm_meter.update(grad_norm)
+        batch_time.update(time.time() - end)
+        ssim_meter.update(ssim_score)
+        end = time.time()
+
+        if idx % config.PRINT_FREQ == 0:
+            lr = optimizer.param_groups[0]['lr']
+            memory_used = torch.cuda.max_memory_allocated() / (1024.0 * 1024.0)
+            etas = batch_time.avg * (num_steps - idx)
+            logger.info(
+                f'Train: [{epoch}/{config.TRAIN.EPOCHS}][{idx}/{num_steps}]\t'
+                f'eta {datetime.timedelta(seconds=int(etas))} lr {lr:.6f}\t'
+                f'time {batch_time.val:.4f} ({batch_time.avg:.4f})\t'
+                f'loss {loss_meter.val:.4f} ({loss_meter.avg:.4f})\t'
+                f'grad_norm {norm_meter.val:.4f} ({norm_meter.avg:.4f})\t'
+                f'ssim {ssim_meter.val:.4f} ({ssim_meter.avg: .4f})\t')
+        l.append(loss_meter.avg)
+        g.append(norm_meter.avg)
+        s.append(ssim_meter.avg)
+    epoch_time = time.time() - start
+    logger.info(f"EPOCH {epoch} training takes {datetime.timedelta(seconds=int(epoch_time))}")
+
+@torch.no_grad()
+def validate(config, data_loader, model, l, s):
+    #criterion = torch.nn.CrossEntropyLoss()
+    #model.eval()
+    
+    batch_time = AverageMeter()
+    loss_meter = AverageMeter()
+    ssim_meter = AverageMeter()
+    end = time.time()
+    #print("bro")
+    for idx, (images, mask, _) in enumerate(data_loader):
+        images = images.cuda(non_blocking=True)
+
+        mask = mask.cuda(non_blocking=True)
+ 
+        # compute output
+        p = []
+        t = []
+        loss, ssim = model(images, mask, t, p)
+        loss_meter.update(loss)
+        ssim_meter.update(ssim)
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+        if idx % config.PRINT_FREQ == 0:
+            memory_used = torch.cuda.max_memory_allocated() / (1024.0 * 1024.0)
+            logger.info(
+                f'Test: [{idx}/{len(data_loader)}]\t'
+                f'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                f'Loss {loss_meter.val:.4f} ({loss_meter.avg:.4f})\t'
+                f'ssim {ssim_meter.val:.4f} ({ssim_meter.avg:.4f})\t')
+        l.append(loss_meter.avg)
+        s.append(ssim_meter.avg)
+    return loss_meter.avg, ssim_meter.avg 
+
+if __name__ == '__main__':
+    _, config = parse_option()
+
+    if config.AMP_OPT_LEVEL != "O0":
+        assert amp is not None, "amp not installed!"
+
+    if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
+        rank = int(os.environ["RANK"])
+        world_size = int(os.environ['WORLD_SIZE'])
+        print(f"RANK and WORLD_SIZE in environ: {rank}/{world_size}")
+    else:
+        rank = -1
+        world_size = -1
+
+    torch.cuda.set_device(config.LOCAL_RANK)
+
+    torch.distributed.init_process_group(backend='nccl', init_method='env://', world_size=world_size, rank=rank)
+
+    torch.distributed.barrier()
+
+    seed = config.SEED + dist.get_rank()
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    cudnn.benchmark = True
+
+    # linear scale the learning rate according to total batch size, may not be optimal
+    linear_scaled_lr = config.TRAIN.BASE_LR * config.DATA.BATCH_SIZE * dist.get_world_size() / 512.0
+    linear_scaled_warmup_lr = config.TRAIN.WARMUP_LR * config.DATA.BATCH_SIZE * dist.get_world_size() / 512.0
+    linear_scaled_min_lr = config.TRAIN.MIN_LR * config.DATA.BATCH_SIZE * dist.get_world_size() / 512.0
+    # gradient accumulation also need to scale the learning rate
+    if config.TRAIN.ACCUMULATION_STEPS > 1:
+        linear_scaled_lr = linear_scaled_lr * config.TRAIN.ACCUMULATION_STEPS
+        linear_scaled_warmup_lr = linear_scaled_warmup_lr * config.TRAIN.ACCUMULATION_STEPS
+        linear_scaled_min_lr = linear_scaled_min_lr * config.TRAIN.ACCUMULATION_STEPS
+    config.defrost()
+    config.TRAIN.BASE_LR = linear_scaled_lr
+    config.TRAIN.WARMUP_LR = linear_scaled_warmup_lr
+    config.TRAIN.MIN_LR = linear_scaled_min_lr
+    config.freeze()
+
+    os.makedirs(config.OUTPUT, exist_ok=True)
+    logger = create_logger(output_dir=config.OUTPUT, dist_rank=dist.get_rank(), name=f"SimMIM_OUTPUT")
+    logger2 = create_logger(output_dir='/rfanfs/pnl-zorro/home/kyler/SimMIM/optimal_output', dist_rank=dist.get_rank(), name=f"OPTIMAL_OUTPUT")
+
+    if dist.get_rank() == 0:
+        path = os.path.join(config.OUTPUT, "config.json")
+        with open(path, "w") as f:
+            f.write(config.dump())
+        logger.info(f"Full config saved to {path}")
+
+    # print config
+    logger.info(config.dump())
+    print('made it here')
+    main(config)
